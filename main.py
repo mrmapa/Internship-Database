56d5611f95cea13e7191fc94cea13cca57182e6d
Derek Mo <derek.mo7203@gmail.com>
2022-01-30 04:41:55 -0500

    new website design, job title input, and drop down

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def load_google_jobs_div(job_title, location):
     getVars = {'q' : job_title, 'l' : location, 'fromage' : 'last', 'sort' : 'date'}
     url = ('https://careers.google.com/jobs/results/?distance=50&q=summer%202022' + urllib.parse.urlencode(getVars))
     page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
+    soup = BeautifulSoup(page.contesnt, "html.parser")
     job_soup = soup.find(id="search-results")
     return job_soup
 
